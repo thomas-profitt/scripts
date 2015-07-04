d67dbfb132eca47dc790e052ed3d127466c48564
Thomas Profitt <thomas@twinenginelabs.com>
2015-07-03 21:58:05 -0500

    Initialize repository

---
+++ lib/terminal_formatting.rb
@@ -0,0 +1,30 @@
+module TerminalFormatting
+
+  def terminal_width
+    begin
+      `tput cols`.to_i
+    rescue
+      80
+    end
+  end
+
+  def left_and_right(left, right)
+    spaces_needed = terminal_width - left.length - right.length
+    spaces_needed = 1 if spaces_needed < 1
+
+    left + (" " * spaces_needed) + right
+  end
+  def center(message)
+    spaces_needed = {}
+    spaces_needed[:left] = (terminal_width / 2) - (message.length / 2)
+    spaces_needed[:right] =
+      terminal_width - (spaces_needed[:left] + message.length)
+
+    unless spaces_needed[:left] < 0 or spaces_needed[:right] < 0
+      (" " * spaces_needed[:left]) + message + (" " * spaces_needed[:right])
+    else
+      message
+    end
+  end
+
+end
