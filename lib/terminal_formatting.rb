3b7f8d48ec355a9706a393d8948786870c8d1d15
Thomas Profitt <tomdprofitt@gmail.com>
2015-09-06 13:38:44 -0500

    Make terminal_formatting not a module

--- lib/terminal_formatting.rb
+++ lib/terminal_formatting.rb
@@ -1,33 +1,29 @@
-module TerminalFormatting
-
-  def terminal_width
-    begin
-      `tput cols`.to_i
-    rescue
-      80
-    end
+def terminal_width
+  begin
+    `tput cols`.to_i
+  rescue
+    80
   end
+end
 
-  def left_and_right(left, right)
-    left = left.to_s
-    right = right.to_s
-    spaces_needed = terminal_width - left.length - right.length
-    spaces_needed = 1 if spaces_needed < 1
+def left_and_right(left, right)
+  left = left.to_s
+  right = right.to_s
+  spaces_needed = terminal_width - left.length - right.length
+  spaces_needed = 1 if spaces_needed < 1
 
-    left + (" " * spaces_needed) + right
+  left + (" " * spaces_needed) + right
+end
+def center(message)
+  message = message.to_s
+  spaces_needed = {}
+  spaces_needed[:left] = (terminal_width / 2) - (message.length / 2)
+  spaces_needed[:right] =
+    terminal_width - (spaces_needed[:left] + message.length)
+
+  unless spaces_needed[:left] < 0 or spaces_needed[:right] < 0
+    (" " * spaces_needed[:left]) + message + (" " * spaces_needed[:right])
+  else
+    message
   end
-  def center(message)
-    message = message.to_s
-    spaces_needed = {}
-    spaces_needed[:left] = (terminal_width / 2) - (message.length / 2)
-    spaces_needed[:right] =
-      terminal_width - (spaces_needed[:left] + message.length)
-
-    unless spaces_needed[:left] < 0 or spaces_needed[:right] < 0
-      (" " * spaces_needed[:left]) + message + (" " * spaces_needed[:right])
-    else
-      message
-    end
-  end
-
 end
