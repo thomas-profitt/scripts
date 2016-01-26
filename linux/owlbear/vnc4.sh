f29223a47aa299273fc079cdc5fb072277fa9c56
Thomas Profitt <tomdprofitt@gmail.com>
2016-01-26 09:59:15 -0600

    Make proper x11vnc scripts for owlbear; remove old

--- linux/owlbear/vnc4.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-x11vnc -geometry 800x480 -listen localhost -solid black -httpport 5900
--- linux/owlbear/vnc6.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-x11vnc -geometry 1024x600 -listen localhost -solid black -httpport 5900
-
