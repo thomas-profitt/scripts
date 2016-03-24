3dfd936ecc992efc98e42e234b84dcde9c6ec339
Thomas Profitt <tomdprofitt@gmail.com>
2016-03-24 14:33:10 -0500

    Move everything around

--- config/pivharv/projects_and_ids.rb
+++ config/pivharv/projects_and_ids.rb
@@ -1,5 +1,8 @@
 def projects_and_ids
   {
-    "shortname" => {"harvest" => 719719719, "pivotal_tracker" => 719719719}
+    "lsi" => {"harvest" => 6988322, "pivotal_tracker" => 1331198},
+    "automaton" => {"harvest" => 10001044, "pivotal_tracker" => 1227610},
+    "fibrebond" => {"harvest" => 9996415, "pivotal_tracker" => 1541325},
+    "arcmail-site" => {"harvest" => 9998103, "pivotal_tracker" => 1556251},
   }
 end
