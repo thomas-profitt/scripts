3be440fe775e21e3bd337d4e6854a46797f523de
Thomas Profitt <tomdprofitt@gmail.com>
2016-04-23 22:42:37 -0500

    Keep pivotal_tracker_api_tokens in pivharv config

---
+++ config/pivharv/projects.rb
@@ -0,0 +1,14 @@
+def projects
+  {
+    "my-great-project" => {
+      "harvest_id" => 719,
+      "pivotal_tracker_id" => 719,
+      "pivotal_tracker_api_token" => "719"
+    },
+    "my-other-great-project" => {
+      "harvest_id" => 213,
+      "pivotal_tracker_id" => 213,
+      "pivotal_tracker_api_token" => "213"
+    }
+  }
+end
--- config/pivharv/projects_and_ids.rb
+++
@@ -1,8 +0,0 @@
-def projects_and_ids
-  {
-    "lsi" => {"harvest" => 6988322, "pivotal_tracker" => 1331198},
-    "automaton" => {"harvest" => 10001044, "pivotal_tracker" => 1227610},
-    "fibrebond" => {"harvest" => 9996415, "pivotal_tracker" => 1541325},
-    "arcmail-site" => {"harvest" => 9998103, "pivotal_tracker" => 1556251},
-  }
-end
